8cd54926a74b4d6c1f46981b30261780bbb46fa5
Keshara <keshara2032@gmail.com>
2024-02-29 20:01:10 -0500

    remove datacollection folder

--- DataCollection/AndroidDevelopment/AV_Streaming_App/camstrm/app/src/main/java/com/example/camstrm/Config.java
+++
@@ -1,5 +0,0 @@
-package com.example.camstrm;
-
-public class Config {
-
-}
--- DataCollection/AndroidDevelopment/AV_Streaming_App/camstrm/app/src/main/java/com/example/camstrm/ImageViewCallback.java
+++
@@ -1,8 +0,0 @@
-package com.example.camstrm;
-
-import java.nio.ByteBuffer;
-
-public interface ImageViewCallback {
-
-    public void updateImageView(byte[] bytes);
-}
