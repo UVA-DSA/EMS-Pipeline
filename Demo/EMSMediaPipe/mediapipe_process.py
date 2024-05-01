0e47cafe9a8efdbd5ecbb1033a13783dff0600f4
Abhishek Satpathy <asatpathy314@gmail.com>
2024-05-01 14:03:42 -0400

    finished extracting mediapipe_process into a separate file

--- Demo/EMSMediaPipe/mediapipe_process.py
+++ Demo/EMSMediaPipe/mediapipe_process.py
@@ -1,14 +1,8 @@
-import multiprocessing
+from multiprocessing import Process
 import mediapipe as mp
-import datetime
-from base64 import b64decode
-import time
-from io import BytesIO
 from PIL import Image
-import numpy as np
-import cv2
 
-# Media Pipe variables
+# Global variables for MediaPipe utilities
 global mp_drawing, mp_drawing_styles, mp_hands, mp_face_mesh
 mp_drawing = mp.solutions.drawing_utils
 mp_drawing_styles = mp.solutions.drawing_styles
@@ -16,55 +10,53 @@ mp_hands = mp.solutions.hands
 mp_face_mesh = mp.solutions.face_mesh
 
 
-class MediaPipeProcess(multiprocessing.Process):
+class MediaPipeProcess(Process):
+    # Constructor for the MediaPipeProcess class
     def __init__(self, input_queue, output_queue):
+        """ Instantiates a separate process for MediaPipe image annotation.
+
+        Args:
+            input_queue (multiprocessing.Queue): input queue for process_image() of QueueImage objects
+            output_queue (multiprocessing.Queue):output queue for annotated images of QueueImage objects
+
+        """
         super(MediaPipeProcess, self).__init__()
-        self.mp_hands = mp.solutions.hands.Hands(
-            max_num_hands=1,
-            model_complexity=0,
-            min_detection_confidence=0.5,
-            min_tracking_confidence=0.5)
-        self.input_queue = input_queue
-        self.output_queue = output_queue
+        self.input_queue = input_queue  # Queue for input images
+        self.output_queue = output_queue  # Queue for output images
+        self.is_running = True
 
-    def run(self):
-        if not self.input_queue.empty():
-            data = self.image_queue.get()
-            image_bytes = b64decode(data)
-            image = Image.open(BytesIO(image_bytes))
-            RGB_img = np.array(image)
-            """ 
-            For interacting with video stream, leaving it out while testing.
-            RGB_img = cv2.cvtColor(RGB_img, cv2.COLOR_RGB2BGR)
-            RGB_img = cv2.rotate(RGB_img, cv2.ROTATE_90_COUNTERCLOCKWISE)
-            RGB_img = cv2.resize(RGB_img, (640, 480))
-            """
-
-    def process_image(self, QueueImage=None):
-        # run below in a another thread
-        global mp_hands
+    # Method to process an image for hand landmark detection
+    def process_image(self, QueueImage):
         hand_detection_results = None
 
-        # color scheme conversions for compatibility
-        QueueImage = cv2.cvtcolor(QueueImage.image, cv2.COLOR_BGR2RGB)
-
-        # hand detection
+        # Initialize the MediaPipe model
         with mp_hands.Hands(
                 max_num_hands=2,
                 model_complexity=0,
                 min_detection_confidence=0.5,
                 min_tracking_confidence=0.5) as hands:
+            # Process the image through the model
             hand_detection_results = hands.process(QueueImage.image)
 
-        QueueImage.image.flags.writeable = True
-        QueueImage.image = cv2.cvtColor(QueueImage.image, cv2.COLOR_RGB2BGR)
-
-        # hand detection annotation
+        # Annotate the image with hand landmarks if any hands are detected
         if hand_detection_results and hand_detection_results.multi_hand_landmarks:
             for hand_landmarks in hand_detection_results.multi_hand_landmarks:
-                mp_drawing.draw_landmarks(QueueImage.image, 
-                                          hand_landmarks, 
-                                          mp_hands.HAND_CONNECTIONS, 
-                                          mp_drawing_styles.get_default_hand_landmarks_style(), 
+                mp_drawing.draw_landmarks(QueueImage.image,
+                                          hand_landmarks,
+                                          mp_hands.HAND_CONNECTIONS,
+                                          mp_drawing_styles.get_default_hand_landmarks_style(),
                                           mp_drawing_styles.get_default_hand_connections_style())
-        return QueueImage
+
+        return QueueImage  # Return the annotated image
+
+    # Overridden run method from Process class
+    def run(self):
+        while self.is_running:  # Loop to process images
+            if not self.input_queue.empty():
+                print("Run begins.")
+                queued_image = self.input_queue.get()
+                print("Image received.")
+                processed_image = self.process_image(QueueImage=queued_image)
+                print("Image processed.")
+                self.output_queue.put(processed_image)
+                print("Image outputted.")
--- Demo/mediapipeTest.py
+++ Demo/mediapipeTest.py
@@ -1,21 +1,41 @@
+# move this file into EMSDev once done testing
+
 import multiprocessing
 import numpy as np
 from EMSMediaPipe.mediapipe_process import MediaPipeProcess
 from PIL import Image
+from classes import QueueImage
+from datetime import datetime
+import cv2
 
 test_image = Image.open("/home/cogems_nist/Downloads/mediapipe_landmark.jpg")
-test_image.show()
+cv2image = np.asarray(test_image)
 
+# Create two queues for interprocess communication
 image_queue = multiprocessing.Queue(maxsize=1)
 results_queue = multiprocessing.Queue()
 
-MediaPipeRunner = MediaPipeProcess(input_queue=image_queue, output_queue=results_queue)
+# Create a MediaPipeWorker object
+MediaPipeWorker = MediaPipeProcess(
+    input_queue=image_queue, output_queue=results_queue)
 
-media_pipe_process = multiprocessing.Process(target=MediaPipeRunner.process_image)
-media_pipe_process.start()
+# media_pipe_process = multiprocessing.Process(target=MediaPipeWorker.process_image_loop)
+# media_pipe_process.start()
 
-img = np.zeros([100,100,3],dtype=np.uint8)
-img.fill(255) # or img[:] = 255
+MediaPipeWorker.start()
+input_image_obj = QueueImage(image=cv2image, timestamp=datetime.now())
+image_queue.put(input_image_obj)
+# results_queue.get(img).show()
 
-image_queue.put(img)
-#results_queue.get(img).show()
+MediaPipeWorker.join()
+img = Image.fromarray(results_queue.get().image)
+img.show()
+
+
+"""
+Add this code to the end of the run() function in mediapipe_process.py in order to test
+the outputs without external scripts.
+                test_image = self.output_queue.get()
+                test_image = Image.fromarray(test_image.image)
+                test_image.show()
+"""
