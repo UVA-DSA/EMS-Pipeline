a853da1b8ea759e3688d6ebc6507d49c9f94b8f6
Abhishek Satpathy <asatpathy314@gmail.com>
2024-06-11 11:43:50 -0400

    added action Recognition in object detector class

--- Demo/EMS_Vision/ObjectDetector.py
+++ Demo/EMS_Vision/ObjectDetector.py
@@ -4,6 +4,7 @@ from classes import DetectionObj
 from .DETR_Engine import DETREngine
 import numpy as np
 import time
+from datetime import datetime
 
 from socketio import Client
 
@@ -17,8 +18,20 @@ class ObjectDetector(Process):
         self.output_queue = output_queue
         self.detr_engine = DETREngine(detr_version)
         self.feedback_client = FeedbackClient()
+        self.detected_objects = [] #list of identified objects 
+        self.action = "Not enough information to determine action." #action for action log
         # self.sio = Client()
         print("ObjectDetector: Initialized")
+
+
+    def actionRecognition(self, obj):
+        self.detected_objects.append(obj)
+        if any (o == 'hands' for o in self.detected_objects):
+            if any (o == 'bvm' for o in self.detected_objects):
+                self.action = 'CPR started' + " " + str(datetime.now())
+            elif any (o == 'defibrillator' for o in self.detected_objects):
+                self.action = 'Defibrillation started' + " " + str(datetime.now())
+        self.feedback_client.send_message(self.action, 'action')
 
 
     def run(self):
@@ -32,22 +45,30 @@ class ObjectDetector(Process):
             # # Do some object detection
             # result_image = self.detr_engine.run_workflow(frame)
             
+            
 
             dummy_obj = DetectionObj({'center_point':'(500, 700)', 'width':'400', 'height':'300'}, 'keyboard', '0.255678475236')
             dummy_obj_dict = dummy_obj.__dict__
-            self.feedback_client.send_message(dummy_obj_dict)
+            self.feedback_client.send_message(dummy_obj_dict, 'feedback')
+
+            self.actionRecognition(dummy_obj)
+
 
             time.sleep(5)
             
             dummy_obj2 = DetectionObj({'center_point':'(800, 500)', 'width':'300', 'height':'200'}, 'computer', '0.455678475236')
             dummy_obj_dict2 = dummy_obj2.__dict__
-            self.feedback_client.send_message(dummy_obj_dict2)
+            self.feedback_client.send_message(dummy_obj_dict2, 'feedback')
+
+            self.actionRecognition(dummy_obj)
 
             time.sleep(3)
 
             dummy_obj2 = DetectionObj({'center_point':'(900, 400)', 'width':'300', 'height':'200'}, 'phone', '0.455678475236')
             dummy_obj_dict2 = dummy_obj2.__dict__
-            self.feedback_client.send_message(dummy_obj_dict2)
+            self.feedback_client.send_message(dummy_obj_dict2, 'feedback')
+
+            self.actionRecognition(dummy_obj2)
 
             time.sleep(3)
     
@@ -58,8 +79,9 @@ class ObjectDetector(Process):
 
             time.sleep(3)
 
+            self.actionRecognition(dummy_obj2)
 
-            
+
 
             # self.feedback_client.send_message(dummy_obj_dict)
             # self.sio.emit('feedback', dummy_obj_dict)
