f9cdbc2ea2bf15f0e74f2e20ca59f8e32d88a2c8
Abhishek Satpathy <asatpathy314@gmail.com>
2024-06-12 12:53:06 -0400

    working model, prior to cleaning

--- Demo/EMS_Vision/ObjectDetector.py
+++ Demo/EMS_Vision/ObjectDetector.py
@@ -28,31 +28,28 @@ class ObjectDetector(multiprocessing.Process):
 
     def actionRecognition(self, obj):
         self.detected_objects.append(obj)
-        print(self.detected_objects)
-        proposedAction = self.action
-        #MAY WANT TO CHANGE THIS TO A SWITCH CASE LATER
+        proposedAction = self.action #self.action is the current action being displayed
+        #MAY WANT TO CHANGE THIS TO A SWITCH CASE LATER, awk in python though
         #HAD TO USE 'person' instead of hand? keeps calling a hand a person
         if any (o == 'person' for o in self.detected_objects):
             if any (o == 'bvm' for o in self.detected_objects):
                 proposedAction = 'CPR started' 
             elif any (o == 'defibrillator' for o in self.detected_objects):
                 proposedAction = 'Defibrillation started'
-            #TAKE OUT LATER, USING FOR ACTION LOG TESTING
+           
+           #TAKE OUT LATER, USING FOR ACTION LOG TESTING
             elif any (o == 'keyboard' for o in self.detected_objects):
                 proposedAction = 'typing' 
-                self.detected_objects = [];
             elif any (o == 'cell phone' for o in self.detected_objects):
                 proposedAction = 'calling' 
-                self.detected_objects = [];
-        if proposedAction != self.action:
+        
+        if proposedAction != self.action: #checks that a new action has began 
             self.action = proposedAction
+            self.detected_objects = []; #reset detected objects, such that we don't trigger the same action after the object is no longer onscreen
             self.feedback_client.send_message(self.action+ " " + str(datetime.now().strftime("%H:%M:%S")), 'action')
 
 
     def run(self):
-        # self.sio.connect(socketio_ipaddr)  # Connect to the Flask-SocketIO server
-        # print("Connected to the server!")
-        # self.sio.emit('message', 'Hello from Object Detector Process!')  # Send a message to the server
         self.detr_engine = DETREngine(detr_version)
         self.feedback_client.start()
         while True:
@@ -64,49 +61,13 @@ class ObjectDetector(multiprocessing.Process):
             
         
             (image_array, objectDetected) = result_image
-            if str(objectDetected) != '[]':
-                for i in range(len(objectDetected)):
-                    self.feedback_client.send_message(objectDetected[i], 'objectFeedback')
-                    if (objectDetected[i].get('obj_name') != 'null'):
-                        self.actionRecognition(objectDetected[i].get('obj_name'))
+            if str(objectDetected) != '[]': #if not null, sometimes it identifies a box with no object detection?
+                for i in range(len(objectDetected)): # loop through output, as there may be more than one object detected
+                    self.feedback_client.send_message(objectDetected[i], 'objectFeedback') #send detected object on objectfeedback channel
+                    self.actionRecognition(objectDetected[i].get('obj_name')) 
 
                 
 
-
-
-    #   
-
-            # # time.sleep(5)
-            
-            # dummy_obj2 = DetectionObj({'center_point':'(800, 500)', 'width':'300', 'height':'200'}, 'computer', '0.455678475236')
-            # dummy_obj_dict2 = dummy_obj2.__dict__
-            # self.feedback_client.send_message(dummy_obj_dict2, 'feedback')
-
-            # self.actionRecognition(dummy_obj)
-
-            # time.sleep(3)
-
-            # dummy_obj2 = DetectionObj({'center_point':'(900, 400)', 'width':'300', 'height':'200'}, 'phone', '0.455678475236')
-            # dummy_obj_dict2 = dummy_obj2.__dict__
-            # self.feedback_client.send_message(dummy_obj_dict2, 'feedback')
-
-            # self.actionRecognition(dummy_obj2)
-
-            # time.sleep(3)
-    
-
-            # dummy_obj2 = DetectionObj({'center_point':'(800, 500)', 'width':'300', 'height':'200'}, 'monitor', '0.455678475236')
-            # dummy_obj_dict2 = dummy_obj2.__dict__
-            # self.feedback_client.send_message(dummy_obj_dict2, 'feedback')
-
-            # time.sleep(3)
-
-            # self.actionRecognition(dummy_obj2)
-
-
-
-            # self.feedback_client.send_message(dummy_obj_dict)
-            # self.sio.emit('feedback', dummy_obj_dict)
             del frame
             self.output_queue.put(result_image)
             print("ObjectDetector: Put frame")
