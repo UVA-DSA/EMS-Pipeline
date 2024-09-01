3011982982a15909f24be14c68366da6f073fce8
Keshara <keshara2032@gmail.com>
2024-09-01 12:20:25 -0400

    add time sync between server and sw and add tool to visualie the offsets'

--- DataCollection/AndroidDevelopment/Smartwatch_app_new/smartwatch_stream/app/src/main/java/com/example/gesturerecognition/SendSensorDataWorker.java
+++ DataCollection/AndroidDevelopment/Smartwatch_app_new/smartwatch_stream/app/src/main/java/com/example/gesturerecognition/SendSensorDataWorker.java
@@ -147,8 +147,9 @@ public class SendSensorDataWorker extends Worker {
                 while ((bytesRead = inputStream.read(buffer)) != -1) {
                     String receivedData = new String(buffer, 0, bytesRead);
                     // Handle received data as needed
-                    Log.d(LOG_TAG, " TCP Client Received: " + receivedData);
-
+                    long serverEpochTime = Long.parseLong(receivedData.split(":")[1]);
+                    Log.d(LOG_TAG, " TCP Client Received Server Epoch: " + serverEpochTime);
+                    SensorData.calculateEpochOffset(serverEpochTime);
                     byte[] data = SensorData();
                     outputStream.write(data);
 
---
+++ DataCollection/Tools/smartwatch_time_offset_vis.py
@@ -0,0 +1,42 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+# Load the CSV file
+file_path = '../Server/test/smartwatch_data/sw_right/sw_data.csv'
+df = pd.read_csv(file_path)
+
+# Calculate the offset between smartwatch epoch time and server epoch time
+df['epoch_offset_ms'] = df['sw_epoch_ms'] - df['server_epoch_ms']
+
+
+# Calculate mean, standard deviation, min, and max of the offset
+mean_offset = df['epoch_offset_ms'].mean()
+std_offset = df['epoch_offset_ms'].std()
+min_offset = df['epoch_offset_ms'].max()
+max_offset = df['epoch_offset_ms'].min()
+
+# Set seaborn style
+sns.set(style="whitegrid")
+
+# Plot the epoch offset
+plt.figure(figsize=(10, 6))
+sns.lineplot(x=df['seq_num'], y=df['epoch_offset_ms'], label='Epoch Offset (ms)', color='blue')
+
+# Add the stats as text on the plot
+plt.text(0.05, 0.60, f"Mean Offset (ms): {mean_offset:.2f}\n"
+                     f"Std Dev (ms): {std_offset:.2f}\n"
+                     f"Min Offset (ms): {min_offset}\n"
+                     f"Max Offset (ms): {max_offset}",
+         horizontalalignment='left', verticalalignment='top', transform=plt.gca().transAxes,
+         bbox=dict(facecolor='white', alpha=0.8))
+
+plt.savefig('./Plots/epoch_offset.png')
+
+plt.xlabel('Sequence Number')
+plt.ylabel('Offset (ms)')
+plt.title('Epoch Offset Between Smartwatch and Server')
+plt.legend()
+plt.grid(True)
+plt.show()
+
