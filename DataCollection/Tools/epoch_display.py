dcfdb60a38cbf82aa53160c9bdc4c495ad2aa403
Hamid Roodabeh <hr.roodabeh@gmail.com>
2024-09-05 09:45:23 -0400

    add epoch display terminal script

---
+++ DataCollection/Tools/epoch_display.py
@@ -0,0 +1,16 @@
+import time
+import os
+
+
+os.system('cls' if os.name == 'nt' else 'clear')
+
+while True:
+    # Get current epoch time in nanoseconds
+    epoch_ns = time.time_ns()
+
+    # Move the cursor to the beginning of the line and overwrite the previous output
+    print(f"\r{epoch_ns}", end="", flush=True)
+
+    # Short delay to avoid overwhelming output
+    time.sleep(0.001)  # 1 millisecond delay
+
