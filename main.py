1e02695f748286ef65496d11ebb9b2194dc12d41
Nitya Khamar <n_khamar@Nityas-MacBook-Air.local>
2025-06-25 22:59:33 -0400

    Added in code to start processes based on button clicks

--- main.py
+++ main.py
@@ -21,9 +21,32 @@ from PyQt5.QtMultimediaWidgets import *
 from PyQt5.QtMultimedia import *
 
 
+from multiprocessing import Event
 
+######################
+"'Process spawning"
+def protocol_process(event):
+    while True:
+        print("Protocol Process/Thread Running")
 
+def feedback_process(event):
+    while True:
+        print("Feedback Process Running")
 
+def speech_process(event):
+    while True:
+        print("Speech Process Running")
+    
+def vision_process(event):
+    while True:
+        print("Vision process Running")
+
+def network_process(event):
+    while True:
+        print("Network Process Running")
+
+######################
+###avoid .terminate since it is not a clean shutdown of the process
 
 
 "'GUI/Window  class"
@@ -36,12 +59,19 @@ class gui_window(QWidget):
         
         
         ##setting up window
-        self.screen_resolution = app.desktop().screenGeometry ##Todo: test to make sure this actually works
-        self.width = screen_resoluton.width()
-        self.height = screen_resoluton.height()
+        self.screen_resolution = self.app.desktop().screenGeometry ##Todo: test to make sure this actually works
+        self.width = self.screen_resoluton.width()
+        self.height = self.screen_resoluton.height()
         self.setWindowTitle('CognitiveEMS Debugging Demo')
 
-
+        ####event creation for each possible process
+        self.protocol_event = Event()
+        self.vision_event = Event()
+        self.feedback_event = Event()
+        self.speech_event = Event()
+        self.network_event = Event()
+        self.processes = []
+        #list of all processes
 
         ##setting up button 
         self.protocol_button_start.clicked.connect(self.start_protocol)
@@ -55,35 +85,62 @@ class gui_window(QWidget):
         self.vision_start_button.clicked.connect(self.vision_start)
         self.vision_stop_button.clicked.connect(self.vision_stop)
 
+
+
 ###function for what happens when a button is clicked
     def start_protocol(self):
+        self.protocol_event.clear()
+        self.protocol = mp.Process(target=protocol_process,args=(self.protocol_event,))
         print("Protocol start")
+        self.processes.append(self.protocol)
+        self.protocol.start()
 
-    def stop_protocol(self):
+    def stop_protocol(self): 
+        self.protocol_event.set()
         print("Protocol stopped")
 
     def vision_start(self):
+        self.vision_event.clear()
+        self.vision = mp.Process(target=vision_process,args=(self.vision_event,))
         print("Vision start")
+        self.processes.append(self.vision)
+        self.vision.start()
 
     def vision_stop(self):
+        self.vision_event.set()
         print("Vision stopped")
 
     def network_start(self):
+        self.network_event.clear()
+        self.network = mp.Process(target=network_process,args=(self.network_event,))
         print("Network started")
+        self.processes.append(self.network)
+        self.network.start()
 
     def network_stop(self):
+        self.network_event.set()
         print("Network stopped")
 
     def speech_start(self):
+        self.speech_event.clear()
+        self.speech = mp.Process(target=speech_process,args=(self.speech_event,))
         print("Speech started")
+        self.processes.append(self.speech)
+        self.speech.start()
 
     def speech_stop(self):
+        self.speech_event.set()
         print("Speech stopped")
 
     def feedback_start(self):
+        self.feedback_event.clear()
+        self.feedback = mp.Process(target=feedback_process,args=(self.feedback_event,))
         print("Feedback started")
-
+        self.processes.append(self.feedback)
+        self.feedback.start()
+        
     def feedback_stop(self):
+        self.feedback_event.set()
         print("Feedback stopped")
 #################################################
 
@@ -105,29 +162,12 @@ if __name__ == "__main__":
     
     print("Starting GUI!")
     application = QApplication(sys.argv)
-    Window = gui_window(application=application)
+    command_queue = Queue()
+    Window = gui_window(application=application,queue=command_queue)
     
     
     ##Debug information
     print(f"width: {Window.width}")
     print(f"height: {Window.height}")
-    ##debug information
-
-
 
     Window.show()
-    #command queue initilaization
-    command_queue  = Queue()
-    #creating processes
-    vision_process = Process(target=spawn_vision)
-    network_process = Process(target=spawn_network)
-    
-
-
-    ##starting processes
-    vision_process.start()
-
-
-
-    #waiting on processes to finish
-    vision_process.join()
